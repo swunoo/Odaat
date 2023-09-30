4d256d476e44ee2befb64b90f6dc8e0bdbc94cd8
swunoo <swunhtetoo@gmail.com>
2023-09-30 21:44:32 +0900

    added basic js

--- src/main/resources/static/js/overview.js
+++ src/main/resources/static/js/overview.js
@@ -4,9 +4,17 @@ ADD_BTN.addEventListener('click', e => {
 });
 
 // Changing modal inputs based on type.
+[DAYS, FROM, TO].forEach(item => item.classList.add('hidden')); // Initial
 TYPE.addEventListener('change', e => {
+    TYPE.children[1].classList.toggle('bg-blue-forced');
     [DAYS, FROM, TO, DEADLINE, ESTIMATED].forEach(item => item.classList.toggle('hidden'));
 });
+
+// Changing disabled attribute for END.
+STATUS.addEventListener('change', e => {
+    const inputBox = END.children[1];
+    inputBox.disabled = !inputBox.disabled;
+})
 
 // Closing the Modal with X.
 CLOSE.addEventListener('click', e => {
