9d34e66021209ee613c918dd6500dea7d70beefc
swunoo <swunhtetoo@gmail.com>
2023-09-30 21:15:41 +0900

    completed modal style

--- src/main/resources/static/js/overview.js
+++ src/main/resources/static/js/overview.js
@@ -1,5 +1,15 @@
 // Clicking the Add button.
 ADD_BTN.addEventListener('click', e => {
-    MODAL.style.display = 'block';
-})
+    MODAL.style.display = 'flex';
+});
+
+// Changing modal inputs based on type.
+TYPE.addEventListener('change', e => {
+    [DAYS, FROM, TO, DEADLINE, ESTIMATED].forEach(item => item.classList.toggle('hidden'));
+});
+
+// Closing the Modal with X.
+CLOSE.addEventListener('click', e => {
+    MODAL.style.display = 'none';
+});
 
